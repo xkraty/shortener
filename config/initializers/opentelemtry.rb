47bd5c266a16a8d729c122b5d8d54d84865d6475
xkraty <christian@campoli.me>
2025-09-01 00:54:28 +0100

    debug

--- config/initializers/opentelemtry.rb
+++ config/initializers/opentelemtry.rb
@@ -1,6 +1,7 @@
 # config/initializers/opentelemetry.rb
 require "opentelemetry/sdk"
 require "opentelemetry/instrumentation/all"
+require "opentelemetry/instrumentation/rails"
 
 OpenTelemetry::SDK.configure do |c|
   c.use_all() # This auto-instruments Rails, HTTP requests, database queries, etc.
