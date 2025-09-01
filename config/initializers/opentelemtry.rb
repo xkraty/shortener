f4a488ebe926f97ef7e10d43a15782dcb57a1b38
xkraty <christian@campoli.me>
2025-09-01 01:07:15 +0100

    boh

--- config/initializers/opentelemtry.rb
+++ config/initializers/opentelemtry.rb
@@ -3,6 +3,25 @@ require "opentelemetry/sdk"
 require "opentelemetry/instrumentation/all"
 require "opentelemetry/instrumentation/rails"
 
+
 OpenTelemetry::SDK.configure do |c|
-  c.use_all() # This auto-instruments Rails, HTTP requests, database queries, etc.
+  c.service_name = "ShortnerZ"
+  c.service_version = "1.0.0"
+
+
+  c.use_all()
+
+  c.logger = OpenTelemetry.logger_provider.logger(name: "ShortnerZ", version: "0.1.0")
+
+  c.logger_provider = OpenTelemetry::SDK::Logs::LoggerProvider.new(
+    resource: OpenTelemetry::SDK::Resources::Resource.create({
+      "service.name" => "ShortnerZ",
+      "host.name" => Socket.gethostname
+    })
+  )
+
+  exporter = OpenTelemetry::Exporter::OTLP::Logs::Exporter.new(endpoint: ENV["OTEL_EXPORTER_OTLP_LOGS_ENDPOINT"])
+
+  processor = OpenTelemetry::SDK::Logs::SimpleLogRecordProcessor.new(exporter)
+  c.logger_provider.add_log_record_processor(processor)
 end
